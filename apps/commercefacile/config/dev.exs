1ab4a1752b3f37ba49d0f28f268717119bcde2c6
thomasdola <thomasdolar@gmail.com>
2017-04-22 13:41:12 +0000

    config db

--- apps/commercefacile/config/dev.exs
+++ apps/commercefacile/config/dev.exs
@@ -3,8 +3,8 @@ use Mix.Config
 # Configure your database
 config :commercefacile, Commercefacile.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "commercefacile_dev",
+  password: "commercefacile_dev",
   database: "commercefacile_dev",
-  hostname: "localhost",
+  hostname: "172.17.0.2",
   pool_size: 10
