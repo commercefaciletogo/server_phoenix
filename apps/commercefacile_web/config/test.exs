5004ab44b2aa582642dfccd646216c21bea645a2
thomasdola <thomasdolar@gmail.com>
2017-04-24 13:39:13 +0000

    add docker service to circleci

--- apps/commercefacile_web/config/test.exs
+++ apps/commercefacile_web/config/test.exs
@@ -3,5 +3,5 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :commercefacile_web, Commercefacile.Web.Endpoint,
-  http: [port: 4001],
+  http: [port: 4000],
   server: false
